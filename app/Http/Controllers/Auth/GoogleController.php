101d2541cf9ac4eb1ac34dd7054b427f351e015d
reClicked <gamper.renato@gmail.com>
2021-03-24 11:47:56 +0100

    feature: add loginCallback method to create user and social account

--- app/Http/Controllers/Auth/GoogleController.php
+++ app/Http/Controllers/Auth/GoogleController.php
@@ -3,16 +3,53 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\SocialAccount;
+use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Laravel\Socialite\Facades\Socialite;
 
 class GoogleController extends Controller
 {
+    /**
+     * Get google login url
+     *
+     * @return JsonResponse
+     */
     public function googleLoginUrl(): JsonResponse
     {
         return response()->json([
             'url' => Socialite::driver('google')->stateless()->redirect()->getTargetUrl(),
+        ]);
+    }
+
+    /**
+     * @return JsonResponse
+     */
+    public function loginCallback(): JsonResponse
+    {
+        $googleUser = Socialite::driver('google')->stateless()->user();
+        $user = null;
+
+        DB::transaction(function () use ($googleUser, &$user) {
+            $socialAccount = SocialAccount::query()->firstOrNew(
+                ['social_id' => $googleUser->getId(), 'social_provider' => 'google'],
+                ['social_name' => $googleUser->getName()]
+            );
+
+            if (!($user = $socialAccount->user)) {
+                $user = User::query()->create([
+                    'email' => $googleUser->getEmail(),
+                    'name' => $googleUser->getName(),
+                ]);
+                $socialAccount->fill(['user_id' => $user->id])->save();
+            }
+        });
+
+        return response()->json([
+            'user' => new UserResource($user),
+            'google_user' => $googleUser,
         ]);
     }
 }
