8b79c18a2d815e8025e6bca62340df474e716449
reClicked <gamper.renato@gmail.com>
2021-04-01 14:38:47 +0200

    db: create schema for subcategories and products table

---
+++ database/migrations/2021_03_31_174404_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 255);
+            $table->longText('description');
+            $table->decimal('price');
+            $table->string('pic_url', 255);
+            $table->foreignId('subcategory_id')->nullable();
+            $table->foreign('subcategory_id')->references('id')->on('subcategories');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
