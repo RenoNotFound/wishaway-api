334cb3b109adcc4499baa61433da69695c067965
reClicked <gamper.renato@gmail.com>
2021-04-07 14:48:40 +0200

    feature: implement getSubcategories method which gives back the subcategories by category

--- app/Http/Controllers/SubcategoryController.php
+++ app/Http/Controllers/SubcategoryController.php
@@ -2,9 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Subcategory;
+use Illuminate\Database\QueryException;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use App\Traits\ApiResponser;
 
 class SubcategoryController extends Controller
 {
-    //
+    use ApiResponser;
+
+    /**
+     * Get all subcategories by category
+     *
+     * @param string $category
+     * @return JsonResponse
+     */
+    public function getSubcategories(string $category): JsonResponse
+    {
+        try {
+            $subcategories = Subcategory::where('category', $category)->get();
+            return $this->success(['subcategories' => $subcategories]);
+
+        } catch (QueryException $e) {
+            return $this->error(500, $e->getMessage(), 'Database error');
+        } catch (\Exception $e) {
+            return $this->error(500, $e->getMessage());
+        }
+    }
+
 }
