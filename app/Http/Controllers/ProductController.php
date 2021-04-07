b670d3a706d713af2614fd823f5b9c63765f860a
reClicked <gamper.renato@gmail.com>
2021-04-07 15:38:58 +0200

    feature: implement getProductsBySubcategory method which gives back the products by subcategory

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,43 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Database\QueryException;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Traits\ApiResponser;
 
 class ProductController extends Controller
 {
-    //
+    use ApiResponser;
+
+    /**
+     * Get all products by subcategory
+     *
+     * @param int $subcategoryId
+     * @return JsonResponse
+     */
+    public function getProductsBySubcategory(int $subcategoryId): JsonResponse
+    {
+        try {
+            $products = DB::table('products')
+                ->join('subcategories', 'products.subcategory_id', '=', "subcategories.id")
+                ->select(
+                    'products.name',
+                    'products.description',
+                    'products.price',
+                    'products.pic_url',
+                    'subcategories.name as subcategory')
+                ->where('products.subcategory_id', $subcategoryId)
+                ->get();
+            return $this->success(['products' => $products]);
+
+        } catch (QueryException $e) {
+            return $this->error(500, $e->getMessage(), 'Database error');
+
+        } catch (\Exception $e) {
+            return $this->error(500, $e->getMessage());
+        }
+    }
+
 }
