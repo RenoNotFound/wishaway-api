337b2349b0c56f6eeb23a6108a3b9ccaf47b7a1e
reClicked <gamper.renato@gmail.com>
2021-04-13 10:41:39 +0200

    feature: add category table seeder to db seeder's run method

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call(CategoryTableSeeder::class);
         $this->call(SubcategoriesTableSeeder::class);
         $this->call(ProductTableSeeder::class);
     }
