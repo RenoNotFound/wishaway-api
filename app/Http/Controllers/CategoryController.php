0649aeb9bdf5e2054898a93db2e7d35b960ff448
reClicked <gamper.renato@gmail.com>
2021-04-13 11:07:21 +0200

    feature: implement get all categories method

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,9 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
+use App\Traits\ApiResponser;
+use Illuminate\Database\QueryException;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    //
+    use ApiResponser;
+
+    /**
+     * Get all categories
+     *
+     * @return JsonResponse
+     */
+    public function getCategories(): JsonResponse
+    {
+        try {
+            $categories = Category::all();
+            return $this->success(['categories' => $categories]);
+
+        } catch (QueryException $e) {
+            return $this->error(500, $e->getMessage(), 'Database error');
+        } catch (\Exception $e) {
+            return $this->error(500, $e->getMessage());
+        }
+    }
+
 }
