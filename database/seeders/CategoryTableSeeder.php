e201cc1eacb0041cfe3cac42663cb60c46fe38f3
reClicked <gamper.renato@gmail.com>
2021-04-13 10:42:35 +0200

    feature: create category table seeder

---
+++ database/seeders/CategoryTableSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CategoryTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('categories')->delete();
+
+        $subcategoriesJson = file_get_contents(
+            database_path() . DIRECTORY_SEPARATOR .
+            'data' . DIRECTORY_SEPARATOR .
+            'categories' . DIRECTORY_SEPARATOR .
+            "categories.json");
+        $subcategories = json_decode($subcategoriesJson);
+
+        foreach ($subcategories as $subcategory) {
+            Category::create([
+                'name' => $subcategory->name,
+                'description' => $subcategory->description
+            ]);
+        }
+    }
+}
